d3948e45818444c2f3f9e3cf24781a763024614b
Helen <150084608+brandoline@users.noreply.github.com>
2026-07-28 20:55:56 -0300

    Layout e funcionabilidade
    Funcionamento de menu, perguntas e atualizações de fontes

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const btnMenu = document.querySelector(".hamburguer");
 const navMobile = document.querySelector(".mobile-nav");
-const pergunta = document.querySelector(".pergunta-resposta");
-const resposta = document.querySelector(".resposta");
+const perguntas = document.querySelectorAll(".pergunta-resposta");
 
 btnMenu.addEventListener('click', () => {
     navMobile.classList.toggle('hidden');
+})
+
+perguntas.forEach(pergunta => {
+    pergunta.addEventListener('click', () => {
+        const resposta = pergunta.querySelector('.resposta');
+
+        resposta.classList.toggle('hidden');
+    })
 })
